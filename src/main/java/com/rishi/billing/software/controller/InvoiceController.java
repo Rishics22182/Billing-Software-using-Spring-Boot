c9768a8cbe669c81932dba46908cff33b9f68978
Rishics22182 <rg10082005@gmail.com>
2026-01-13 11:46:24 +0530

    Initial commit in branch db-jdbc.

--- src/main/java/com/rishi/billing/software/controller/InvoiceController.java
+++ src/main/java/com/rishi/billing/software/controller/InvoiceController.java
@@ -1,32 +1,27 @@
 package com.rishi.billing.software.controller;
-
 import com.rishi.billing.software.dto.InvoiceRequestDTO;
 import com.rishi.billing.software.entity.Invoice;
 import com.rishi.billing.software.service.InvoiceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
-@RequestMapping("/invoices")   // Base URL: /invoices
+@RequestMapping("/invoices")
 public class InvoiceController {
 
     private final InvoiceService invoiceService;
 
-    // Constructor injection
     public InvoiceController(InvoiceService invoiceService) {
         this.invoiceService = invoiceService;
     }
 
-    // Create invoice
     @PostMapping
     public ResponseEntity<Invoice> createInvoice(@RequestBody InvoiceRequestDTO requestDTO) {
         Invoice createdInvoice = invoiceService.createInvoice(requestDTO);
         return ResponseEntity.ok(createdInvoice);
     }
 
-    // Get invoice by ID
     @GetMapping("/{invoiceId}")
     public ResponseEntity<Invoice> getInvoiceById(@PathVariable long invoiceId) {
         Invoice invoice = invoiceService.getOrderById(invoiceId);
@@ -36,7 +31,6 @@ public class InvoiceController {
         return ResponseEntity.ok(invoice);
     }
 
-    // Get all invoices
     @GetMapping
     public ResponseEntity<List<Invoice>> getAllInvoices() {
         List<Invoice> invoices = invoiceService.getAllInvoice();
